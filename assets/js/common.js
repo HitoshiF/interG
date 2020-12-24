77b699b7f535ac7bb0b16e070245b73415d74e3e
hitoshi <brs.com.55555@gmail.com>
2020-12-25 02:17:19 +0900

    update:test

---
+++ assets/js/common.js
@@ -0,0 +1,165 @@
+// スクロールアニメーション
+ScrollReveal().reveal('.fadeIn', { 
+  duration: 1000, 
+  viewFactor: 0.2, 
+});
+ScrollReveal().reveal('.bottom', { 
+  duration: 1000, 
+  viewFactor: 0.2,
+  distance: "30%",
+});
+
+// PC
+ScrollReveal().reveal('.right', { 
+  origin: "right",
+  duration: 800, 
+  viewFactor: 0.2,
+  distance: "50%",
+  mobile: false,
+});
+ScrollReveal().reveal('.left', { 
+  origin: "left",
+  duration: 800, 
+  viewFactor: 0.2,
+  distance: "50%",
+  mobile: false,
+});
+
+ScrollReveal().reveal('.rt_01', { 
+  origin: "right",
+  duration: 800, 
+  viewFactor: 0.2,
+  distance: "700px",
+  mobile: false,
+});
+ScrollReveal().reveal('.rt_02', { 
+  origin: "right",
+  duration: 800, 
+  viewFactor: 0.2,
+  distance: "700px",
+  delay: 100,
+  mobile: false,
+});
+ScrollReveal().reveal('.rt_03', { 
+  origin: "right",
+  duration: 800, 
+  viewFactor: 0.2,
+  distance: "700px",
+  delay: 200,
+  mobile: false,
+});
+ScrollReveal().reveal('.rt_04', { 
+  origin: "right",
+  duration: 800, 
+  viewFactor: 0.2,
+  distance: "700px",
+  delay: 300,
+  mobile: false,
+});
+ScrollReveal().reveal('.rt_05', { 
+  origin: "right",
+  duration: 800, 
+  viewFactor: 0.2,
+  distance: "700px",
+  delay: 400,
+  mobile: false,
+});
+ScrollReveal().reveal('.rt_06', { 
+  origin: "right",
+  duration: 800, 
+  viewFactor: 0.2,
+  distance: "700px",
+  delay: 500,
+  mobile: false,
+});
+ScrollReveal().reveal('.rt_07', { 
+  origin: "right",
+  duration: 800, 
+  viewFactor: 0.2,
+  distance: "700px",
+  delay: 600,
+  mobile: false,
+});
+ScrollReveal().reveal('.rt_08', { 
+  origin: "right",
+  duration: 800, 
+  viewFactor: 0.2,
+  distance: "700px",
+  delay: 700,
+  mobile: false,
+});
+
+// SP
+ScrollReveal().reveal('.left_sp', { 
+  origin: "left",
+  duration: 800, 
+  viewFactor: 0.2,
+  distance: "50%",
+  desktop: false,
+});
+
+ScrollReveal().reveal('.rt_01', { 
+  origin: "right",
+  duration: 800, 
+  viewFactor: 0.2,
+  distance: "100px",
+  desktop: false,
+});
+ScrollReveal().reveal('.rt_02', { 
+  origin: "right",
+  duration: 800, 
+  viewFactor: 0.2,
+  distance: "100px",
+  delay: 100,
+  desktop: false,
+});
+ScrollReveal().reveal('.rt_03', { 
+  origin: "right",
+  duration: 800, 
+  viewFactor: 0.2,
+  distance: "100px",
+  delay: 200,
+  desktop: false,
+});
+ScrollReveal().reveal('.rt_04', { 
+  origin: "right",
+  duration: 800, 
+  viewFactor: 0.2,
+  distance: "100px",
+  delay: 300,
+  desktop: false,
+});
+ScrollReveal().reveal('.rt_05', { 
+  origin: "right",
+  duration: 800, 
+  viewFactor: 0.2,
+  distance: "100px",
+  delay: 400,
+  desktop: false,
+});
+ScrollReveal().reveal('.rt_06', { 
+  origin: "right",
+  duration: 800, 
+  viewFactor: 0.2,
+  distance: "100px",
+  delay: 500,
+  desktop: false,
+});
+ScrollReveal().reveal('.rt_07', { 
+  origin: "right",
+  duration: 800, 
+  viewFactor: 0.2,
+  distance: "100px",
+  delay: 600,
+  desktop: false,
+});
+ScrollReveal().reveal('.rt_08', { 
+  origin: "right",
+  duration: 800, 
+  viewFactor: 0.2,
+  distance: "100px",
+  delay: 700,
+  desktop: false,
+});
+
+

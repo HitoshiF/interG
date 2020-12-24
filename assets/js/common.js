1ca5109645c5b776cdf917110b90781d5f26c02b
hitoshi <brs.com.55555@gmail.com>
2020-12-25 03:52:58 +0900

    update:js

--- assets/js/common.js
+++ assets/js/common.js
@@ -6,158 +6,158 @@ ScrollReveal().reveal('.fadeIn', {
 ScrollReveal().reveal('.bottom', { 
   duration: 1000, 
   viewFactor: 0.2,
-  distance: "30%",
+  distance: '30%',
 });
 
 // PC
 ScrollReveal().reveal('.right', { 
-  origin: "right",
+  origin: 'right',
   duration: 800, 
   viewFactor: 0.2,
-  distance: "50%",
+  distance: '50%',
   mobile: false,
 });
 ScrollReveal().reveal('.left', { 
-  origin: "left",
+  origin: 'left',
   duration: 800, 
   viewFactor: 0.2,
-  distance: "50%",
+  distance: '50%',
   mobile: false,
 });
 
 ScrollReveal().reveal('.rt_01', { 
-  origin: "right",
+  origin: 'right',
   duration: 800, 
   viewFactor: 0.2,
-  distance: "700px",
+  distance: '700px',
   mobile: false,
 });
 ScrollReveal().reveal('.rt_02', { 
-  origin: "right",
+  origin: 'right',
   duration: 800, 
   viewFactor: 0.2,
-  distance: "700px",
+  distance: '700px',
   delay: 100,
   mobile: false,
 });
 ScrollReveal().reveal('.rt_03', { 
-  origin: "right",
+  origin: 'right',
   duration: 800, 
   viewFactor: 0.2,
-  distance: "700px",
+  distance: '700px',
   delay: 200,
   mobile: false,
 });
 ScrollReveal().reveal('.rt_04', { 
-  origin: "right",
+  origin: 'right',
   duration: 800, 
   viewFactor: 0.2,
-  distance: "700px",
+  distance: '700px',
   delay: 300,
   mobile: false,
 });
 ScrollReveal().reveal('.rt_05', { 
-  origin: "right",
+  origin: 'right',
   duration: 800, 
   viewFactor: 0.2,
-  distance: "700px",
+  distance: '700px',
   delay: 400,
   mobile: false,
 });
 ScrollReveal().reveal('.rt_06', { 
-  origin: "right",
+  origin: 'right',
   duration: 800, 
   viewFactor: 0.2,
-  distance: "700px",
+  distance: '700px',
   delay: 500,
   mobile: false,
 });
 ScrollReveal().reveal('.rt_07', { 
-  origin: "right",
+  origin: 'right',
   duration: 800, 
   viewFactor: 0.2,
-  distance: "700px",
+  distance: '700px',
   delay: 600,
   mobile: false,
 });
 ScrollReveal().reveal('.rt_08', { 
-  origin: "right",
+  origin: 'right',
   duration: 800, 
   viewFactor: 0.2,
-  distance: "700px",
+  distance: '700px',
   delay: 700,
   mobile: false,
 });
 
 // SP
 ScrollReveal().reveal('.left_sp', { 
-  origin: "left",
+  origin: 'left',
   duration: 800, 
   viewFactor: 0.2,
-  distance: "50%",
+  distance: '50%',
   desktop: false,
 });
 
 ScrollReveal().reveal('.rt_01', { 
-  origin: "right",
+  origin: 'right',
   duration: 800, 
   viewFactor: 0.2,
-  distance: "100px",
+  distance: '100px',
   desktop: false,
 });
 ScrollReveal().reveal('.rt_02', { 
-  origin: "right",
+  origin: 'right',
   duration: 800, 
   viewFactor: 0.2,
-  distance: "100px",
+  distance: '100px',
   delay: 100,
   desktop: false,
 });
 ScrollReveal().reveal('.rt_03', { 
-  origin: "right",
+  origin: 'right',
   duration: 800, 
   viewFactor: 0.2,
-  distance: "100px",
+  distance: '100px',
   delay: 200,
   desktop: false,
 });
 ScrollReveal().reveal('.rt_04', { 
-  origin: "right",
+  origin: 'right',
   duration: 800, 
   viewFactor: 0.2,
-  distance: "100px",
+  distance: '100px',
   delay: 300,
   desktop: false,
 });
 ScrollReveal().reveal('.rt_05', { 
-  origin: "right",
+  origin: 'right',
   duration: 800, 
   viewFactor: 0.2,
-  distance: "100px",
+  distance: '100px',
   delay: 400,
   desktop: false,
 });
 ScrollReveal().reveal('.rt_06', { 
-  origin: "right",
+  origin: 'right',
   duration: 800, 
   viewFactor: 0.2,
-  distance: "100px",
+  distance: '100px',
   delay: 500,
   desktop: false,
 });
 ScrollReveal().reveal('.rt_07', { 
-  origin: "right",
+  origin: 'right',
   duration: 800, 
   viewFactor: 0.2,
-  distance: "100px",
+  distance: '100px',
   delay: 600,
   desktop: false,
 });
 ScrollReveal().reveal('.rt_08', { 
-  origin: "right",
+  origin: 'right',
   duration: 800, 
   viewFactor: 0.2,
-  distance: "100px",
+  distance: '100px',
   delay: 700,
   desktop: false,
 });
